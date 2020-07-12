e892ecd20fc0d65a9adaa221ae0ca74441d5d794
hjzheng818 <hjzheng818@126.com>
2020-07-12 11:45:31 +0800

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+q
+Asrc/main/java/cncc/hjzheng/example/SpringbootDemoApplication.java,8\2\8235bc5ba4b862c5b21168615a3afd1386c6959f
+r
+Bsrc/main/java/cncc/hjzheng/example/controller/HelloController.java,2\5\25876161aa1eae736ee8d1fda866343779eb4264
---
+++ src/main/java/cncc/hjzheng/example/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cncc.hjzheng.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** @author: hjzheng */
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String hello() {
+        return "index";
+    }
+
+}
